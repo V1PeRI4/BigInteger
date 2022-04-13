f81d6bfdee3303fc594bb4f41d88a193352acdc6
user <user@C1005>
2022-04-13 15:23:30 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp10/MyBigInteger.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class MyBigInteger
+    {
+        int Positive = 1;
+
+        public MyBigInteger(string num, int positive)
+        {
+            for (int i = 0; i < num.Length; i++)
+            {
+                string s = (num[i].ToString());
+                numbers.Add(byte.Parse(s));
+            }
+
+            Positive = positive;
+        }
+
+        List<byte> numbers = new List<byte>();
+
+
+        public void Print()
+        {
+            foreach (int i in numbers)
+                Console.Write(i);
+        }
+
+        public static bool operator >(MyBigInteger a, MyBigInteger b)
+        {
+           return (a.CompareTo(b)) == 1;
+        }
+
+        public static bool operator <(MyBigInteger a, MyBigInteger b)
+        {
+            return (b.CompareTo(a)) == 1;
+        }
+
+
+        private int CompareTo(MyBigInteger other)
+        {
+            if (Positive != other.Positive) return Positive.CompareTo(other.Positive);
+            if (numbers.Count != other.numbers.Count) return numbers.Count.CompareTo(other.numbers.Count);
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] > other.numbers[i])
+                    return 1;
+
+                if (numbers[i] < other.numbers[i])
+                    return -1;
+            }
+            return 0;
+
+        }
+
+   
+  
+    }
+}
---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ConsoleApp10
+{
+    internal class Program
+
+    {
+        static void Main(string[] args)
+        {
+            
+            BigInteger sdf = new BigInteger(123123123);
+            MyBigInteger myBigInteger = new MyBigInteger("123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123123", 1);
+            MyBigInteger myBigInteger2 = new MyBigInteger("121231231231231231231231231231231231231231231231231231231231231231231231233", 1);
+
+
+            Console.WriteLine(myBigInteger > myBigInteger2); 
+            //myBigInteger.Print();
+
+            Console.ReadKey();
+        }
+    }
+}
